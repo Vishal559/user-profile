137585b238380f19dc76447ad7c641abbbfcbadb
ayeshaqaisar <56070872+ayeshaqaisar@users.noreply.github.com>
2020-10-18 23:08:19 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//eslint-disable-next-line
+import User, * as user from './components/User'
+//eslint-disable-next-line
 import * as serviceWorker from './serviceWorker';
 
+function App() {
+  return (
+    <div className="App">
+   <User />
+    </div>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
